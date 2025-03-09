8432c41ca2176525d9135aa2f4b6cd4dbc86730c
kristinadd <kristina@productboard.com>
2025-03-09 17:16:53 +0100

    Abstract Factory

--- abstractfactory/AbstractShapeFactory.java
+++ abstractfactory/AbstractShapeFactory.java
@@ -12,3 +12,19 @@ public interface AbstractShapeFactory {
     return null;
   }
 }
+
+// The static method get is associated with the interface itself, 
+// not with any instance of an implementing class. This means you 
+// can call it directly as AbstractShapeFactory.get(someType). This 
+// method contains logic to return a specific factory instance 
+// (using the abstract factory design pattern) based on the 
+// provided shape type.
+
+// *******************************************************************
+
+// The abstract method create(Shape.Type type) provides a contract 
+// that any implementing class must fulfill by supplying its own implementation.
+
+// The static method get(Shape.Type type) provides a utility method that 
+// can be used to obtain an instance of the appropriate factory without 
+// needing an already constructed instance of an implementing class.
